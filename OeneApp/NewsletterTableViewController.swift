c185801fd00bed993234b2e8ccb76186ac0a371f
Lucas Cerro <lucascerro@gmail.com>
2015-04-30 18:42:22 -0300

    Cleaned up code
    removed old/commented code

--- OeneApp/NewsletterTableViewController.swift
+++ OeneApp/NewsletterTableViewController.swift
@@ -9,10 +9,6 @@
 import UIKit
 
 class NewsletterTableViewController: UITableViewController {
-    
-    
-    
-    //JSON newsletters: https://www.kimonolabs.com/api/8mghmria?apikey=6xMrJDEUW8UeMXQUSg3bD2ulvwGN8saQ
     //MARK: refresh function
     func refreshNewsletters() {
         var endpoint = NSURL(string: "https://www.kimonolabs.com/api/8mghmria?apikey=6xMrJDEUW8UeMXQUSg3bD2ulvwGN8saQ")
@@ -31,116 +27,8 @@ class NewsletterTableViewController: UITableViewController {
                     }
                 }
         }
-//        let urlString = NSURL(string: "http://tinyletter.com/Oene/archive")
-//        let rssUrlRequest:NSURLRequest = NSURLRequest(URL:urlString!)
-//        let queue:NSOperationQueue = NSOperationQueue()
-        
-//        NSURLConnection.sendAsynchronousRequest(rssUrlRequest, queue: queue) {
-//            (response, data, error) -> Void in
-//            self.xmlParser = NSXMLParser(data: data)
-//            self.xmlParser.delegate = self
-//            println("vai chamar o parse!")
-//            self.xmlParser.parse()
-//        }
     }
     
-    //MARK: Parse: variables
-//    var xmlParser: NSXMLParser!
-//    
-//    var entryTitle: String!
-//    var entryDate: String!
-//    var entryURL: NSURL!
-//    var entryDescription: String!
-//    
-//    var currentNewsletterParsedElement = String()
-//    var weAreInsideAnLI = false
-//    var weAreInsideAnAnchor = false
-    
-    //MARK: Parse: functions
-//    func parser(parser: NSXMLParser,
-//        didStartElement elementName: String,
-//        namespaceURI: String?,
-//        qualifiedName: String?,
-//        attributes attributeDict: [NSObject : AnyObject]){
-//            if elementName == "li" {
-//                weAreInsideAnLI = true
-//                println("we're inside an <li>")
-//            }
-//            if weAreInsideAnLI {
-//                if elementName == "a" {
-//                    entryURL = attributeDict["href"] as! NSURL
-//                    weAreInsideAnAnchor = true
-//                }
-//                switch elementName {
-//                case "span":
-//                    if weAreInsideAnAnchor {
-//                        entryTitle = String()
-//                        currentNewsletterParsedElement = "title"
-//                    } else {
-//                        entryDate = String()
-//                        currentNewsletterParsedElement = "date"
-//                    }
-//                case "p":
-//                    entryDescription = String()
-//                    currentNewsletterParsedElement = "description"
-//                default: break
-//                }
-//            }
-//    }
-//    
-//    func parser(parser: NSXMLParser,
-//        foundCharacters string: String?) {
-//            if weAreInsideAnLI {
-//                switch currentNewsletterParsedElement {
-//                    case "title":
-//                    entryTitle = entryTitle + string!
-//                    case "date":
-//                    entryDate = entryDate + string!
-//                    case "description":
-//                    entryDescription = entryDescription + string!
-//                default: break
-//                }
-//            }
-//    }
-//
-//    func parser(parser: NSXMLParser,
-//        didEndElement elementName: String,
-//        namespaceURI: String?,
-//        qualifiedName qName: String?) {
-//            if weAreInsideAnLI {
-//                switch elementName {
-//                    case "title":
-//                    currentNewsletterParsedElement = ""
-//                    case "date":
-//                    currentNewsletterParsedElement = ""
-//                    case "description":
-//                    currentNewsletterParsedElement = ""
-//                    case "a":
-//                    weAreInsideAnAnchor = false
-//                    case "li":
-//                    var entryNewsletter = Newsletter()
-//                    entryNewsletter.newsletterTitle = entryTitle
-//                    entryNewsletter.newsletterDescription = entryDescription
-//                    entryNewsletter.newsletterDate = entryDate
-//                    entryNewsletter.newsletterURL = entryURL
-//                    newsletters.append(entryNewsletter)
-//                    weAreInsideAnLI = false
-//                default: break
-//                }
-//            }
-//    }
-//    
-//    func parserDidEndDocument(parser: NSXMLParser){
-//        dispatch_async(dispatch_get_main_queue(), { () -> Void in
-//            println(newsletters.count)
-//            println("title 0 = \(newsletters[0].newsletterTitle)")
-//            println("description 0 = \(newsletters[0].newsletterDescription)")
-//            println("title 5 = \(newsletters[5].newsletterTitle)")
-//            println("description 5 = \(newsletters[5].newsletterDescription)")
-//            self.tableView.reloadData()
-//        })
-//    }
-
 
     //MARK: ViewController Lifecycle
     override func viewDidLoad() {
@@ -148,9 +36,6 @@ class NewsletterTableViewController: UITableViewController {
         self.tableView.rowHeight = UITableViewAutomaticDimension
         self.tableView.estimatedRowHeight = 40.0
         refreshNewsletters()
-        
-        // self.titlesTableView.estimatedRowHeight = 40.0
-
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
--- OeneApp/PodcastTableViewController.swift
+++ OeneApp/PodcastTableViewController.swift
@@ -20,7 +20,6 @@ class PodcastTableViewController: UITableViewController, NSXMLParserDelegate {
             //3
             self.xmlParser = NSXMLParser(data: data)
             self.xmlParser.delegate = self
-//            println("vai chamar o parse!")
             self.xmlParser.parse()
         }
     }
@@ -41,102 +40,73 @@ class PodcastTableViewController: UITableViewController, NSXMLParserDelegate {
     
     
     //MARK: Parse: funcions
-    
-    //FIRST, isolate the podcasts from rest of XML with <item> tag
-//    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [NSObject : AnyObject]) {
-//        if elementName == "item" {
-    
-            func parser(parser: NSXMLParser,
-                didStartElement elementName: String,
-                namespaceURI: String?,
-                qualifiedName: String?,
-                attributes attributeDict: [NSObject : AnyObject]){
-                    if elementName == "item" {
-                        weAreInsideAnItem = true
-//                        println("we're inside an item")
-//                        println("\(xmlParser.columnNumber)")
-
-                    }
-                    if weAreInsideAnItem {
-                        switch elementName {
-                            case "title":
-//                                println("inside an item and found a title")
-                                entryTitle = String()
-//                                println("entryTitle = \(entryTitle)")
-                                currentParsedElement = "title"
-            //                case "pubDate":
-            //                    entryDate = String()
-            //                    currentParsedElement = "pudDate"
-            //                case "enclosure":
-            //                    entryURL = PodcastLinkInfo()
-            //                    currentParsedElement = "enclosure"
-            //                case "itunes:duration":
-            //                    entryDuration = String()
-            //                    currentParsedElement = "itunes:duration"
-            //                case "itunes:subtitle":
-            //                    entrySubtitle = String()
-            //                    currentParsedElement = "itunes:subtitle"
-                            case "itunes:summary":
-                                entryDescription = String()
-//                                println("inside an item and found a description")
-                                currentParsedElement = "itunes:summary"
-                        default: break
-                    }
-                    }
+    func parser(parser: NSXMLParser,
+        didStartElement elementName: String,
+        namespaceURI: String?,
+        qualifiedName: String?,
+        attributes attributeDict: [NSObject : AnyObject]){
+            if elementName == "item" {
+                weAreInsideAnItem = true
             }
-
-            func parser(parser: NSXMLParser,
-                foundCharacters string: String?) {
-                    if weAreInsideAnItem {
-                        if currentParsedElement == "title" {
-                            entryTitle = entryTitle + string!
-//                            println("++++atribuiu title ao string: \(entryTitle)")
-                        }
-                        if currentParsedElement == "itunes:summary" {
-                            entryDescription = entryDescription + string!
-//                            println("++++atribuiu desc ao string: \(entryDescription)")
-
-                        }
-                    }
+            if weAreInsideAnItem {
+                switch elementName {
+                    case "title":
+                        entryTitle = String()
+                        currentParsedElement = "title"
+    //                case "pubDate":
+    //                    entryDate = String()
+    //                    currentParsedElement = "pudDate"
+    //                case "enclosure":
+    //                    entryURL = PodcastLinkInfo()
+    //                    currentParsedElement = "enclosure"
+    //                case "itunes:duration":
+    //                    entryDuration = String()
+    //                    currentParsedElement = "itunes:duration"
+    //                case "itunes:subtitle":
+    //                    entrySubtitle = String()
+    //                    currentParsedElement = "itunes:subtitle"
+                    case "itunes:summary":
+                        entryDescription = String()
+                        currentParsedElement = "itunes:summary"
+                default: break
             }
-            //3
-            func parser(parser: NSXMLParser,
-                didEndElement elementName: String,
-                namespaceURI: String?,
-                qualifiedName qName: String?) {
-                    if weAreInsideAnItem {
-                        if elementName == "title" {
-//                            podcasts.append(entryPodcast)
-//                            podcasts[podcasts.count-1].podcastTitle = entryTitle
-                            currentParsedElement = ""
-//                            println("====defined entryPodcast title = \(entryTitle)")
-                        }
-                        if elementName == "itunes:summary" {
-//                            println("\(podcasts.count-1)")
-//                            podcasts[podcasts.count-1].podcastDescription = entryDescription
-                            currentParsedElement = ""
-//                            println("====defined entryPodcast description = \(entryDescription)")
-                        }
-                    }
-                    if elementName == "item" {
-                        weAreInsideAnItem = false
-                        var entryPodcast = Podcast()
-                        entryPodcast.podcastTitle = entryTitle
-                        entryPodcast.podcastDescription = entryDescription
-//                        println("****appended new podcast to podcasts[]")
-                        podcasts.append(entryPodcast)
-//                        println("podcasts.count = \(podcasts.count)")
-                    }
             }
+    }
+
+    func parser(parser: NSXMLParser,
+        foundCharacters string: String?) {
+            if weAreInsideAnItem {
+                if currentParsedElement == "title" {
+                    entryTitle = entryTitle + string!
+                }
+                if currentParsedElement == "itunes:summary" {
+                    entryDescription = entryDescription + string!
+                }
+            }
+    }
+    func parser(parser: NSXMLParser,
+        didEndElement elementName: String,
+        namespaceURI: String?,
+        qualifiedName qName: String?) {
+            if weAreInsideAnItem {
+                if elementName == "title" {
+                    currentParsedElement = ""
+                }
+                if elementName == "itunes:summary" {
+                    currentParsedElement = ""
+                }
+            }
+            if elementName == "item" {
+                weAreInsideAnItem = false
+                var entryPodcast = Podcast()
+                entryPodcast.podcastTitle = entryTitle
+                entryPodcast.podcastDescription = entryDescription
+                podcasts.append(entryPodcast)
+            }
+    }
     
-    //4
     func parserDidEndDocument(parser: NSXMLParser){
         dispatch_async(dispatch_get_main_queue(), { () -> Void in
-//            println(podcasts.count)
-//            println("title 0 = \(podcasts[0].podcastTitle)")
-//            println("description 0 = \(podcasts[0].podcastDescription)")
-//            println("title 5 = \(podcasts[5].podcastTitle)")
-//            println("description 5 = \(podcasts[5].podcastDescription)")
             self.tableView.reloadData()
         })
     }
@@ -146,11 +116,8 @@ class PodcastTableViewController: UITableViewController, NSXMLParserDelegate {
     //MARK: ViewController Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.edgesForExtendedLayout = UIRectEdge.None
-//        self.extendedLayoutIncludesOpaqueBars = false
-//        self.automaticallyAdjustsScrollViewInsets = false
         self.tableView.rowHeight = UITableViewAutomaticDimension
-        self.tableView.estimatedRowHeight = 40.0
+        self.tableView.estimatedRowHeight = 60.0
         refreshPodcasts()
         
 
